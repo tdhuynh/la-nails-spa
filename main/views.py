1b88599c8492ad5c0ed1e2db4c777005e204b5f3
tdhuynh <tommyhuynh93@gmail.com>
2017-03-29 12:43:49 -0400

    removed blog html, added template views for pages

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
