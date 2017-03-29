79d2ec7c7756712280415e7bed56e5cd4b7d7abc
tdhuynh <tommyhuynh93@gmail.com>
2017-03-29 12:46:00 -0400

    renamed homeview class

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 
-class HomeView(TemplateView):
+class IndexView(TemplateView):
     template_name = 'index.html'
 
 
