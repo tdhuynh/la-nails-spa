731902b4055061b17c3b2a5b06fba092795e8cd3
tdhuynh <tommyhuynh93@gmail.com>
2017-03-29 12:11:46 -0400

    initial commit, created project

---
+++ la_nails_spa/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for la_nails_spa project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "la_nails_spa.settings")
+
+application = get_wsgi_application()
